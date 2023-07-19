77c628856ede85706def1d1639f0e5ac7a9953eb
Laetitia Frost <laetitia@diefrosts.de>
2023-07-19 15:05:00 +0200

    added pe2

--- performance_eval.R
+++ performance_eval.R
@@ -9,9 +9,6 @@ evaluate_splits_rep_ho(test_results2)
 get_avg_cms(test_results, F)
 get_avg_cms(test_results2, F)
 
-
-
-
 ################################################ Status: Tested and done
 get_avg_cms <- function(cms, cm_extracted) {
   # fit_hists <- lapply(results, function(result)
@@ -54,5 +51,35 @@ inspect_transitions <- function(predictions, splits, predictions_extracted){
   avg_cms <- get_avg_cms(cms, T)
   cmb_cms <- c(avg_cms, cms)
   cmb_cms
+}
+
+################## For Testing
+# predictions, x, curr_length_existing, lags_existing, test_indcs, data
+# splits <- create_splits_nested(df)
+# splits[[1]][[1]][[3]]
+# oos <- create_oos_forecast(test_results2[[1]][[2]], test_results2[[1]], F, T, splits[[1]][[1]][[3]], data)
+
+create_oos_forecast <- function(predictions, x, curr_length_existing, lags_existing, test_indcs, data){
+  predictions_oos <- predictions
+  test_data <- lapply(data, function(x) deepregression:::subset_array(x, test_indcs))
+  for (row in 1:nrow(predictions_oos)) {
+    dp <- lapply(test_data, function(x) deepregression:::subset_array(x, row))
+    predictions_oos[row, ] <- x %>% predict(dp)
+    predicted_class_oos <- names(which.max(predictions_oos[row, ]))
+    predicted_classes_oos[row] <- predicted_class_oos
+    if (row > 1 & curr_length_existing){
+      if(predicted_class_oos == predicted_classes_oos[row-1])
+        test_data$curr_length[row] <- test_data$curr_length[[row - 1]] + 1
+      else
+        test_data$curr_length[row] <- 1
+    }
+    if (lags_existing) {
+      for (lag in 1:6) {
+        if (lag + row <= nrow(predictions_oos))
+          test_data[paste0("lag_", lag)][[1]][row + lag] <-as.factor(predicted_class_oos)
+      }
+    }
+  }
+  list(predictions_oos, predicted_classes_oos)
 }
 
